e194e83ca898c7f257fc94c580e9aeb983bf1e78
Christian Deacon <gamemann@gflclan.com>
2023-04-13 22:41:58 -0500

    Organize front-end code.

--- resources/js/Pages/index.tsx
+++ resources/js/Pages/index.tsx
@@ -1,13 +1,9 @@
 import { Link, Head } from '@inertiajs/react';
 import React from 'react';
 
-export default function Welcome({ auth, laravelVersion, phpVersion }) {
-    const telNet: number = 3301;
-    
+export default function Welcome({ auth, laravelVersion, phpVersion }) {    
     return (
-        
         <>
-            {telNet}
             <Head title="Welcome" />
             <div className="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
                 <div className="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
--- routes/web.php
+++ routes/web.php
@@ -17,22 +17,12 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
+    return Inertia::render('index', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
         'laravelVersion' => Application::VERSION,
         'phpVersion' => PHP_VERSION,
     ]);
-});
-
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 require __DIR__.'/auth.php';
