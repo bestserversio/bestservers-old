75740f62b050161e1d3021e86cbc45dc3a7358bb
Christian Deacon <gamemann@gflclan.com>
2023-04-17 19:21:42 -0500

    Add meta config for meta information in head.

---
+++ config/meta.php
@@ -0,0 +1,11 @@
+<?php
+    return [
+        'defaults' => [
+            'title' => 'Best Servers',
+            'description' => 'Find your favorite community and modded servers!',
+            'image' => '/images/bestservers_banner.png',
+            'key_words' => 'game, servers, browser, finder, community, mods',
+            'robots' => 'index, follow',
+            'web_type' => 'website'
+        ]
+    ];
