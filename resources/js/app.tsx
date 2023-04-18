f2b678199a41b8775aa0b71574991712abc997ae
Christian Deacon <gamemann@gflclan.com>
2023-04-17 19:21:02 -0500

    Don't use app name in title.

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -6,10 +6,8 @@ import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import React from 'react';
 
-const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
-
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
+    title: (title) => `${title}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.tsx`, import.meta.glob('./Pages/**/*.tsx')),
     setup({ el, App, props }) {
         const root = createRoot(el);
