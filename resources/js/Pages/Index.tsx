1716fd835bcbd7333712bdf69dfc59425407ffc3
Christian Deacon <gamemann@gflclan.com>
2023-04-17 19:21:27 -0500

    Start implementing meta/head information in Index.

--- resources/js/Pages/Index.tsx
+++ resources/js/Pages/Index.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
 
-export default function Welcome({ test }) {    
+import HeadInfo from '../Components/head';
+
+export default function Index({ 
+    test, 
+    meta 
+}) {    
     return (
         <>
+            <HeadInfo
+                title={meta.title}
+                description={meta.description}
+                image={meta.image}
+                key_words={meta.key_words}
+                robots={meta.robots}
+                web_type={meta.web_type}
+            />
             <p>Hello! Test # is {test}</p>
         </>
     );
