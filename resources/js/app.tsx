d629714c789b3ae692e7e7d1a9fcfe6561af2808
Christian Deacon <gamemann@gflclan.com>
2023-04-23 20:10:35 -0500

    Seperate CSS code into additional files for organization.

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,6 +1,10 @@
 import './bootstrap';
 import '../css/app.css';
 
+import '../css/header.css';
+import '../css/forms.css';
+import '../css/filters.css';
+
 import { createRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
