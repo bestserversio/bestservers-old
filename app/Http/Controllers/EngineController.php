b0c1b45c9d384916036075941a68a304f08561a8
Christian Deacon <gamemann@gflclan.com>
2023-04-29 20:04:17 -0500

    Make progress on engine controller.

--- app/Http/Controllers/EngineController.php
+++ app/Http/Controllers/EngineController.php
@@ -6,6 +6,9 @@ use Illuminate\Http\Request;
 
 use Inertia\Inertia;
 
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
+
 class EngineController extends Controller
 {
     /**
@@ -19,12 +22,45 @@ class EngineController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(Request $request)
     {
         $meta = gen_meta();
 
+        // See if we have existing values.
+        $vals = [
+            'name' => $request->old('name', ''),
+            'name_short' => $request->old('name_short', ''),
+            'description' => $request->old('description', ''),
+            'is_a2s' => $request->old('is_a2s', false),
+            'is_discord' => $request->old('is_discord', false)
+        ];
+
+        // Generate CSRF token for protection.
+        $csrf = csrf_token();
+
+        $error = null;
+
+        $errors = $request->session()->get('errors');
+
+        if ($errors) {
+            $errors = $errors->engine;
+
+            
+            $error = [];
+            $error['title'] = "Unable To Create Engine";
+
+            foreach ($errors->all() as $err) {
+                $error['message'] = $err;
+
+                break;
+            }
+        }
+
         return Inertia::render('Engines/New', [
-            'meta' => $meta
+            'meta' => $meta,
+            'values' => $vals,
+            'csrf' => $csrf,
+            'errors' => $error
         ]);
     }
 
@@ -33,7 +69,20 @@ class EngineController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // Validation
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'name_short' => 'required|string|max:64',
+            'description' => 'string',
+            'is_a2s' => 'required|boolean',
+            'is_discord' => 'required|boolean'
+        ], [
+            'name.required' => 'The engine name is required.',
+            'name_short.required' => 'The engine short name is required.',
+            'is_a2s.required' => 'The engine is A2S field is required.'
+        ])->validateWithBag('engine');
+
+        return redirect()->back()->withErrors($validator);
     }
 
     /**
