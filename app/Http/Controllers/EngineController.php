5263f2922a831321ab59924776eda3ea34c0c342
Christian Deacon <gamemann@gflclan.com>
2023-05-24 01:10:52 -0500

    Add support for editing and deleting.

--- app/Http/Controllers/EngineController.php
+++ app/Http/Controllers/EngineController.php
@@ -116,9 +116,98 @@ class EngineController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(string $id, Request $request)
     {
-        //
+        $meta = gen_meta();
+
+        // Retrieve existing engine.
+        $engine = Engine::firstWhere('id', (int) $id);
+
+        if (!$engine) {
+            return Inertia::render('Error/ResourceNotFound', [
+                'meta' => $meta
+            ]);
+        }
+
+        // Let's set up our data.
+        $vals = [
+            'name' => $engine->name,
+            'name_short' => $engine->name_short,
+            'description' => $engine->description,
+            'is_a2s' => $engine->is_a2s,
+            'is_discord' => $engine->is_discord
+        ];
+
+        // If we have old data, this indicates we had errors, but we want to make sure our new data isn't wiped.
+        $oldData = $request->old('name', null);
+
+        if ($oldData) {
+            $vals['name'] = $oldData;
+        }
+
+        $oldData = $request->old('name_short', null);
+
+        if ($oldData) {
+            $vals['name_short'] = $oldData;
+        }
+
+        $oldData = $request->old('description', null);
+
+        if ($oldData) {
+            $vals['description'] = $oldData;
+        }
+
+        $oldData = $request->old('is_a2s', null);
+
+        if ($oldData) {
+            $vals['is_a2s'] = $oldData;
+        }
+
+        $oldData = $request->old('is_discord', null);
+
+        if ($oldData) {
+            $vals['is_discord'] = $oldData;
+        }
+
+        // Successful edit.
+        $success = false;
+
+        // Generate CSRF token for protection.
+        $csrf = csrf_token();
+
+        // Handle validation errors if any.
+        $errors = null;
+
+        $errors_session = $request->session()->get('errors');
+
+        if ($errors_session && ($errors_session = $errors_session->engine)) {
+            $errors = [];
+            $errors_all = $errors_session->all();
+
+            // Make sure we have errors stored in array.
+            if (count($errors_all)) {
+                /* To do: Determine field name and use that in title. */
+                foreach ($errors_session->all() as $num => $err) {
+                    $errors[] = [
+                        'title' => 'Error #' . ($num + 1),
+                        'message' => $err
+                    ];
+                }
+            } else {
+                // If we don't, it indicates a successful entry.
+                $success = true;
+            }
+        }
+
+        return Inertia::render('Engines/New', [
+            'meta' => $meta,
+            'id' => $id,
+            'values' => $vals,
+            'csrf' => $csrf,
+            'errors' => $errors,
+            'success' => $success,
+            'btn_text' => 'Edit!'
+        ]);
     }
 
     /**
@@ -126,7 +215,38 @@ class EngineController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        Log::debug("Updating engine with ID " . (int)$id);
+        $resp = redirect()->back();
+
+        // Validation
+        $validator = $this->makeValidation($request);
+
+        // Check if validated and if so, insert into database.
+        if ($validator) {
+            $engine = Engine::find((int) $id);
+
+            if (!$engine) {
+                $resp = $resp->with('error', 'Engine not found.');
+
+                Log::debug("Engine not found!");
+            } else {
+                // Update.
+                $engine->update([
+                    'name' => $validator['name'],
+                    'name_short' => $validator['name_short'],
+                    'description' => $validator['description'],
+                    'is_a2s' => $request->has('is_a2s'),
+                    'is_discord' => $request->has('is_discord')
+                ]);
+
+                $engine->save();
+
+                Log::debug("Updating engine!");
+                Log::debug(print_r($engine, true));
+            }
+        }
+
+        return $resp->withErrors($validator);
     }
 
     /**
@@ -134,6 +254,28 @@ class EngineController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $resp = redirect()->back();
+
+        $engine = Engine::find((int) $id);
+
+        if (!$engine) {
+            $resp = $resp->with('error', 'Engine not found.');
+        } else {
+            $engine->delete();
+        }
+
+        return $resp;
+    }
+
+    private function makeValidation(Request $request) 
+    {
+        return Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'name_short' => 'required|string|max:64',
+            'description' => 'string|nullable'
+        ], [
+            'name.required' => 'The engine name is required.',
+            'name_short.required' => 'The engine short name is required.'
+        ])->validateWithBag('engine');
     }
 }
