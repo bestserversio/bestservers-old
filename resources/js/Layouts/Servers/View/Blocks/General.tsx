b9078c4b459f9570ab5138aacbd6a585f68f8bd9
Christian Deacon <gamemann@gflclan.com>
2023-06-02 14:31:22 -0500

    Start on server view front-end.

---
+++ resources/js/Layouts/Servers/View/Blocks/General.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { type ServerType } from '@/Components/Types'
+
+const General: React.FC<{
+    server: ServerType
+}> = ({
+    server
+}) => {
+    return (
+        <div className="server-view-block-general">
+
+        </div>
+    );
+}
+
+export default General;
---
+++ resources/js/Layouts/Servers/View/View.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { type ServerType } from "@/Components/Types"
+
+const View: React.FC<{
+    server: ServerType
+}> = ({
+    server
+}) => {
+    return (
+        <div className="server-view">
+
+
+        </div>
+    );
+}
+
+export default View;
